62e65e46c4c75308ee4c3358d4c72c44423dee57
Vikram Hegde <vikramhegde07@gmail.com>
2025-08-01 22:37:44 +0530

    trying to solve the send mail issue in mobile devices

--- client/src/components/Feedback.tsx
+++ client/src/components/Feedback.tsx
@@ -26,7 +26,7 @@ const FeedbackForm = () => {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
-                    Accept: "application/json",
+                    "Accept": "application/json",
                 },
                 body: JSON.stringify(formData),
             });
